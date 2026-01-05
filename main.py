f7924802c90988fc5f4e1a808392cc682ee246e1
Yi An <yiannie726@gmail.com>
2026-01-05 18:36:30 +0800

    flatten path

--- main.py
+++ main.py
@@ -19,6 +19,39 @@ def init_mrt_graph():
 		mrt_map.add_edge(intervals.iloc[i,0], intervals.iloc[i,1], int(intervals.iloc[i,2]))
 	return mrt_map
 
+def flatten_path(graph, path):
+	'''
+	compare curr and next, flatten timings and paths on the same line
+	#specify interchanges
+	'''
+	#st.info
+	prev=0
+	total_time=0
+	total_total=0
+	wait=1
+	for i in range(len(path)-1):
+		name1=graph.get_station_name(path[i])
+		name2=graph.get_station_name(path[i+1])
+		if graph.same_line(path[i],path[i+1]): # at the end of a line(either interchange or end of list), print the total time for that segment.
+			if prev==0:
+				print(name1, end="")
+				prev=1
+			st.write(f" ➔ {name2}",end="")
+			total_time+=graph.neighbours_list[path[i]][path[i+1]]
+		elif name1==name2:
+			st.write(f' Time: {total_time} min')
+			total_total+=total_time
+			total_time=0
+			transfer_time=graph.neighbours_list[path[i]][path[i+1]]
+			st.write(f"Transfer at {name1} from {path[i]} to {path[i+1]}. Time: {transfer_time} min") #print transfer time.
+			st.write("Maximum waiting time: 5 min")
+			wait+=1
+			total_total+=transfer_time
+			prev=0
+	#at end of line, check and clear if not empty
+	st.write(f'Time: {total_total} to {total_total+(wait*5)} min')
+	st.write('\n')
+
 mrt_map=init_mrt_graph()
 
 #UI component
@@ -32,39 +65,6 @@ if st.button("Calculate"):
 	
 	#display
 	st.subheader("SUggested Route")
-	st.metric(label="",value=f"Shortest time from {start} ({mrt_map.get_station_code(start)}) to {end} ({mrt_map.get_station_code(start)}) is {time} minutes.")
+	st.write(f"Shortest time from {start} ({mrt_map.get_station_code(start)}) to {end} ({mrt_map.get_station_code(start)}) is {time} minutes.")
 
-	path_str = " ➔ ".join(path)
-	st.info(path_str)
-
-# def flatten_path(self, path):
-# 		'''
-# 		compare curr and next, flatten timings and paths on the same line
-# 		#specify interchanges
-# 		'''
-# 		prev=0
-# 		total_time=0
-# 		total_total=0
-# 		wait=1
-# 		for i in range(len(path)-1):
-# 			name1=self.get_station_name(path[i])
-# 			name2=self.get_station_name(path[i+1])
-# 			if self.same_line(path[i],path[i+1]): # at the end of a line(either interchange or end of list), print the total time for that segment.
-# 				if prev==0:
-# 					print(name1, end="")
-# 					prev=1
-# 				print(f" -> {name2}",end="")
-# 				total_time+=self.neighbours_list[path[i]][path[i+1]]
-# 			elif name1==name2:
-# 				print(f' Time: {total_time} min')
-# 				total_total+=total_time
-# 				total_time=0
-# 				transfer_time=self.neighbours_list[path[i]][path[i+1]]
-# 				print(f"Transfer at {name1} from {path[i]} to {path[i+1]}. Time: {transfer_time} min") #print transfer time.
-# 				print("Maximum waiting time: 5 min")
-# 				wait+=1
-# 				total_total+=transfer_time
-# 				prev=0
-# 		#at end of line, check and clear if not empty
-# 		print(f'Time: {total_total} to {total_total+(wait*5)} min')
-# 		print('\n')
+	flatten_path(mrt_map, path)
